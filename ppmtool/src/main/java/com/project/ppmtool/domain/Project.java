538e515d55808155f648d8955a952a4536715dcf
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-15 15:09:30 -0500

    CrateProject works

--- ppmtool/src/main/java/com/project/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/project/ppmtool/domain/Project.java
@@ -2,6 +2,7 @@ package com.project.ppmtool.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.Date;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
