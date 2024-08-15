2352eca1989db89af186a393b637eca2c37957c9
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-15 15:54:18 -0500

    API response Error handing using annotation

--- ppmtool/src/main/java/com/project/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/project/ppmtool/web/ProjectController.java
@@ -2,9 +2,11 @@ package com.project.ppmtool.web;
 
 import com.project.ppmtool.domain.Project;
 import com.project.ppmtool.services.ProjectService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,11 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity< Project > createNewProject( @RequestBody Project project ){
+    public ResponseEntity< ? > createNewProject(@Valid @RequestBody Project project, BindingResult result ){
+
+        if(result.hasErrors()){
+            return new ResponseEntity<String>("Object is Empty" , HttpStatus.BAD_REQUEST);
+        }
         Project addProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity <>(project, HttpStatus.CREATED);
     }
