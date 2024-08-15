d9f5b86b7c341360643b049993a1ffb087bf6c1d
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-15 14:08:21 -0500

    Repository Interface and application.property file

---
+++ ppmtool/src/main/java/com/project/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.project.ppmtool.repository;
+
+import com.project.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository< Project, Long > {
+
+    @Override
+    Iterable < Project > findAllById ( Iterable < Long > longs );
+
+}
