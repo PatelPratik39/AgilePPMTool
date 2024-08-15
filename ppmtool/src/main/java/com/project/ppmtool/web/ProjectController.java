28fdb4087d187c8a50282052c9506d4e932b9f5d
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-15 18:22:57 -0500

    custom Error Handling using HashMap and for loop with FieldError class

--- ppmtool/src/main/java/com/project/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/project/ppmtool/web/ProjectController.java
@@ -7,10 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.*;
+
 
 @RestController
 @RequestMapping("/api/project")
@@ -21,10 +26,20 @@ public class ProjectController {
 
     @PostMapping("")
     public ResponseEntity< ? > createNewProject(@Valid @RequestBody Project project, BindingResult result ){
+/**
+ * below logic will send specific errors using a key value pair using HashMap function and iterating using for loop
+ * over FieldError class methods
+ */
+
 
         if(result.hasErrors()){
-            return new ResponseEntity<String>("Object is Empty" , HttpStatus.BAD_REQUEST);
+            Map <String , String> errorMap = new HashMap <>();
+            for(FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity< Map <String , String> >(errorMap, HttpStatus.BAD_REQUEST);
         }
+
         Project addProject = projectService.saveOrUpdateProject(project);
         return new ResponseEntity <>(project, HttpStatus.CREATED);
     }
