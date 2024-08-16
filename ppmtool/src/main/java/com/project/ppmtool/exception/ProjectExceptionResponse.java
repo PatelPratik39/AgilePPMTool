42a3fa5915ab59f7fed19930c8f2e15f564fe576
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-15 19:02:04 -0500

    Exception handing in CreatePorject -POST

---
+++ ppmtool/src/main/java/com/project/ppmtool/exception/ProjectExceptionResponse.java
@@ -0,0 +1,14 @@
+package com.project.ppmtool.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectExceptionResponse {
+
+    private String projectIdentifier ;
+
+}
--- ppmtool/src/main/java/com/project/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/project/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.project.ppmtool.services;
 
 import com.project.ppmtool.domain.Project;
+import com.project.ppmtool.exception.ProjectIdException;
 import com.project.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,18 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject( Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+//            // Check if the project identifier already exists
+//            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+//            if (existingProject != null && (project.getId() == null || !existingProject.getId().equals(project.getId()))) {
+//                throw new ProjectIdException("Project ID '" + project.getProjectIdentifier() + "' already exists");
+//            }
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID" + project.getProjectIdentifier().toUpperCase() + " already Exists");
+        }
+
     }
 
 }
