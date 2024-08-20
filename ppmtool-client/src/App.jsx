9e0d790de38d919860473f11b56ff80956cd49c6
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-19 22:13:23 -0500

    Create Project Form created

--- ppmtool-client/src/App.jsx
+++ ppmtool-client/src/App.jsx
@@ -1,13 +1,23 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
     <>
-      <Header />
-      <Dashboard />
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
