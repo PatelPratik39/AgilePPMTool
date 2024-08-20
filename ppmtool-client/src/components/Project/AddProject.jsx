ab78943d790c499e96cb1270b7ae92631a67bb47
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-19 22:13:37 -0500

    Create Project Form created using react

---
+++ ppmtool-client/src/components/Project/AddProject.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class AddProject extends Component {
+
+    constructor(){
+        super()
+
+        this.state = {
+            
+        }
+
+    }
+  render() {
+    return (
+      <>
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h5 className="display-4 text-center">Create Project form</h5>
+                <hr />
+                <form>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg "
+                      placeholder="Project Name"
+                      name="projectName"
+                    />
+                    <br />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Unique Project ID"
+                      name="projectIdentifier"
+                    />
+                    <br />
+                  </div>
+
+                  <div className="form-group">
+                    <textarea
+                      className="form-control form-control-lg"
+                      placeholder="Project Description"
+                      name="description"
+                    ></textarea>
+                    <br />
+                  </div>
+                  <h6>Start Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="start_date"
+                    />
+                    <br />
+                  </div>
+                  <h6>Estimated End Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="end_date"
+                    />
+                  </div>
+
+                  <input
+                    type="submit"
+                    className="btn btn-primary btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default AddProject;
